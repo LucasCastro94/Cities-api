af75f88798211a9bea94db6651f9cfe4b7e23cab
LucasCastro <lucascastro_94@hotmail.com>
2023-02-03 09:26:52 -0300

    first commit

---
+++ src/main/java/com/lucascastro/cities_api_nearby_ddd/CitiesApiNearbyDddApplication.java
@@ -0,0 +1,13 @@
+package com.lucascastro.cities_api_nearby_ddd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitiesApiNearbyDddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitiesApiNearbyDddApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucascastro/cities_api_nearby_ddd/CitiesApiNearbyDddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucascastro.cities_api_nearby_ddd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitiesApiNearbyDddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
